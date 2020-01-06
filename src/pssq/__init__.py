3a7828a0732d9f6569eaed7f2e058e448c1942bb
Alexander Ptakhin <aptakhin@nanosemantics.ai>
2020-01-06 13:07:57 +0300

    Merge update and delete changes from other project

--- src/pssq/__init__.py
+++ src/pssq/__init__.py
@@ -86,6 +86,8 @@ class Q:
 
     _M_SELECT = "SELECT"
     _M_INSERT = "INSERT"
+    _M_UPDATE = "UPDATE"
+    _M_DELETE = "DELETE"
 
     @staticmethod
     def SELECT(fields=None):
@@ -93,7 +95,11 @@ class Q:
         if fields is None:
             fields = "*"
         elif isinstance(fields, str):
-            fields = fields.split(",")
+            fields = [f.strip() for f in fields.split(",")]
+
+        if fields != "*" and not isinstance(fields, list):
+            fields = [fields]
+
         q.select_fields = fields
         return q
 
@@ -103,17 +109,34 @@ class Q:
         q._INSERT_TO = table
         return q
 
+    @staticmethod
+    def UPDATE(table):
+        q = Q(main_cmd=Q._M_UPDATE)
+        q._UPDATE = table
+        return q
+
+    @staticmethod
+    def DELETE(table):
+        q = Q(main_cmd=Q._M_DELETE)
+        q._DELETE = table
+        return q
+
     def __init__(self, main_cmd=None):
         self.main_cmd = main_cmd
-        assert main_cmd in (None, Q._M_SELECT, Q._M_INSERT,)
-        self._FROM = None
-        self._INSERT_TO = None
+        assert main_cmd in (None, Q._M_SELECT, Q._M_INSERT, Q._M_UPDATE, Q._M_DELETE)
         self.select_fields = None
         self.update_fields = None
+
+        self._FROM = None
+        self._INSERT_TO = None
+        self._UPDATE = None
+        self._DELETE = None
+
         self._WHERE = []
         self._SET = []
         self._ORDER = []
         self._RETURNING = []
+        self._ON_CONFLICT_DO_NOTHING = None
 
     def FROM(self, table):
         self._FROM = table
@@ -147,6 +170,10 @@ class Q:
         self._RETURNING.extend(args)
         return self
 
+    def ON_CONFLICT_DO_NOTHING(self):
+        self._ON_CONFLICT_DO_NOTHING = True
+        return self
+
     def END(self, debug_print=False):
         q = ""
         q_args = ()
@@ -156,13 +183,17 @@ class Q:
 
         if self.main_cmd == self._M_SELECT:
             if self.select_fields not in (None, "*"):
-                q += " " + ",".join(quoted(f) for f in self.select_fields)
+                q += " " + ",".join(self._format_select_field(f) for f in self.select_fields)
             else:
                 q += " *"
 
             q += " FROM " + quoted(self._FROM)
         elif self.main_cmd == self._M_INSERT:
             q += " INTO " + quoted(self._INSERT_TO)
+        elif self.main_cmd == self._M_UPDATE:
+            q += " " + quoted(self._UPDATE)
+        elif self.main_cmd == self._M_DELETE:
+            q += " " + quoted(self._DELETE)
 
         if self.main_cmd == self._M_INSERT:
             set_fields = []
@@ -171,13 +202,24 @@ class Q:
             for wh in self._SET:
                 add_insert, add_value, add_args, idx = wh.format(idx)
                 if not add_insert:
-                    raise RuntimeError(f"Can't handle arg {wh} with INSERT")
+                    raise RuntimeError("Can't handle arg %s with INSERT" % wh)
                 set_fields.append(add_insert)
                 set_values.append(add_value)
                 set_args += add_args
 
             q += " (" + ", ".join(set_fields) + ")"
             q += " VALUES (" + ", ".join(set_values) + ")"
+            q_args += set_args
+
+        if self.main_cmd == self._M_UPDATE:
+            set_fields = []
+            set_args = ()
+            for wh in self._SET:
+                add_insert, add_value, add_args, idx = wh.format(idx)
+                set_fields.append("%s=%s" % (add_insert, add_value))
+                set_args += add_args
+
+            q += " SET " + ", ".join(set_fields)
             q_args += set_args
 
         if self._WHERE:
@@ -187,9 +229,9 @@ class Q:
             for wh in self._WHERE:
                 add_insert, add_value, add_args, idx = wh.format(idx)
                 if add_insert:
-                    where_fields.append(f"{add_insert}={add_value}")
+                    where_fields.append("{add_insert}={add_value}".format(add_insert=add_insert, add_value=add_value))
                 else:
-                    where_fields.append(f"{add_value}")
+                    where_fields.append("{add_value}".format(add_value=add_value))
                 where_args += add_args
             q += " " + " AND ".join(where_fields)
             q_args += where_args
@@ -200,6 +242,9 @@ class Q:
                 order_fields.append(quoted(field) + (" DESC" if order == -1 else ""))
             q += " ORDER BY " + ", ".join(order_fields)
 
+        if self._ON_CONFLICT_DO_NOTHING:
+            q += " ON CONFLICT DO NOTHING"
+
         if self._RETURNING:
             q += " RETURNING " + ", ".join(quoted(f) if f != "*" else f for f in self._RETURNING)
 
@@ -207,6 +252,13 @@ class Q:
             print('Q: %s; %s' % (q, q_args))
 
         return q, q_args
+
+    @staticmethod
+    def _format_select_field(field):
+        if isinstance(field, Q.Unsafe):
+            return field.value
+        else:
+            return quoted(field)
 
 
 def test_q():
@@ -225,6 +277,9 @@ def test_q():
     assert Q.SELECT().FROM("foo").WHERE("a=ANY({})", [2, 4]).WHERE("b={}", 3).END() == ('SELECT * FROM "foo" WHERE a=ANY($1) AND b=$2', ([2, 4], 3))
     assert Q.SELECT().FROM("foo").WHERE("a<={}", 3).END() == ('SELECT * FROM "foo" WHERE a<=$1', (3,))
     assert Q.SELECT("boo").FROM("foo").WHERE("a=5").END() == ('SELECT "boo" FROM "foo" WHERE a=5', ())
+    assert Q.SELECT(Q.Unsafe("foo() as qoo")).FROM("foo").WHERE(a=5).END() == ('SELECT foo() as qoo FROM "foo" WHERE "a"=$1', (5,))
+    assert Q.SELECT([Q.Unsafe("foo() as qoo")]).FROM("foo").WHERE(a=5).END() == ('SELECT foo() as qoo FROM "foo" WHERE "a"=$1', (5,))
+    assert Q.SELECT([Q.Unsafe("foo() as qoo"), "boolka"]).FROM("foo").WHERE(a=5).END() == ('SELECT foo() as qoo,"boolka" FROM "foo" WHERE "a"=$1', (5,))
 
     assert Q.INSERT("foo").SET(a=5).END() == ('INSERT INTO "foo" ("a") VALUES ($1)', (5,))
     assert Q.INSERT("foo").SET(q=Q.Unsafe("4"), w=Q.Unsafe("now()"), a=5).END() == ('INSERT INTO "foo" ("q", "w", "a") VALUES (4, now(), $1)', (5,))
@@ -233,10 +288,16 @@ def test_q():
     assert Q.SELECT().FROM("foo").ORDER("b", -1).END() == ('SELECT * FROM "foo" ORDER BY "b" DESC', ())
 
     assert Q.INSERT("foo").SET(a=5).RETURNING("a").END() == ('INSERT INTO "foo" ("a") VALUES ($1) RETURNING "a"', (5,))
-    assert Q.INSERT("foo").SET(a=5).RETURNING("*").END() == ('INSERT INTO "foo" ("a") VALUES ($1) RETURNING *', (5,))
 
     # Tests based on preserved order of kwargs are valid on Python 3.6+ only
     assert Q.INSERT("foo").SET(a=5, b=7).END() == ('INSERT INTO "foo" ("a", "b") VALUES ($1, $2)', (5, 7))
+
+    assert Q.UPDATE("foo").SET(a=5, b=7).END() == ('UPDATE "foo" SET "a"=$1, "b"=$2', (5, 7))
+    assert Q.UPDATE("foo").SET(a=5, b=7).WHERE(a=2).END() == ('UPDATE "foo" SET "a"=$1, "b"=$2 WHERE "a"=$3', (5, 7, 2))
+    assert Q.UPDATE("foo").SET(a=Q.Unsafe("now()")).WHERE(a=2).END() == ('UPDATE "foo" SET "a"=now() WHERE "a"=$1', (2,))
+
+    assert Q.DELETE("foo").END() == ('DELETE "foo"', ())
+    assert Q.DELETE("foo").WHERE(a=2).END() == ('DELETE "foo" WHERE "a"=$1', (2,))
 
 
 if __name__ == "__main__":
